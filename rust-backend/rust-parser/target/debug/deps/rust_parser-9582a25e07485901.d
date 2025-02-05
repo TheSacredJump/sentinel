7005f94f273e66e375ed89aba89705d8db75fc55
jwaguirr <jwaguirre.23@gmail.com>
2025-02-05 13:53:55 -0600

    Added rust directory along with postgres db docker container

---
+++ rust-backend/rust-parser/target/debug/deps/rust_parser-9582a25e07485901.d
@@ -0,0 +1,5 @@
+/Users/jwaguirr/Documents/Projects/sentinel/rust-backend/rust-parser/target/debug/deps/rust_parser-9582a25e07485901: src/main.rs
+
+/Users/jwaguirr/Documents/Projects/sentinel/rust-backend/rust-parser/target/debug/deps/rust_parser-9582a25e07485901.d: src/main.rs
+
+src/main.rs:
---
+++ rust-backend/rust-parser/target/debug/rust-parser.d
@@ -0,0 +1,1 @@
+/Users/jwaguirr/Documents/Projects/sentinel/rust-backend/rust-parser/target/debug/rust-parser: /Users/jwaguirr/Documents/Projects/sentinel/rust-backend/rust-parser/src/main.rs
